a2d6fb5068ad363a1c57d88a5bac0c49273bd142
Ryan Mower <ryancmower@gmail.com>
2021-06-17 17:52:26 -0500

    Skeleton for account added.

--- HostASkier/account/views.py
+++ HostASkier/account/views.py
@@ -11,24 +11,24 @@ import requests
 def register(request):
 
     # determines whether the form is being submitted or visited
-    # is a POST request when being submitted
-    if request.method == 'POST':
-        form = AccountRegisterForm(request.POST)
+    # # is a POST request when being submitted
+    # if request.method == 'POST':
+    #     form = AccountRegisterForm(request.POST)
 
-        # check validity of form
-        if form.is_valid():
+    #     # check validity of form
+    #     if form.is_valid():
 
-            # saves user
-            pre_save = form.save(commit=False)
-            if (set_lat_lon(pre_save)):
-                #displays success message and redirects to homepage
-                pre_save.save()
-                username = form.cleaned_data.get('username')
-                messages.success(request, f'{username}\'s account created successfully')
-                return redirect('login')
-            else:
-                messages.error(request, 'Please enter a valid address.')
+    #         # saves user
+    #         pre_save = form.save(commit=False)
+    #         if (set_lat_lon(pre_save)):
+    #             #displays success message and redirects to homepage
+    #             pre_save.save()
+    #             username = form.cleaned_data.get('username')
+    #             messages.success(request, f'{username}\'s account created successfully')
+    #             return redirect('login')
+    #         else:
+    #             messages.error(request, 'Please enter a valid address.')
 
-    else:
-        form = AccountRegisterForm()
+    # else:
+    #     form = AccountRegisterForm()
     return render(request, 'users/register.html', {'form':form})
