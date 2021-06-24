fc43144385137e4e3e7f0e10616edc0e3aa2b1d4
charlierehder <charlesreh1577@gmail.com>
2021-06-23 20:31:20 -0500

    bug fixed where users could view match/ page w/o logging in

--- HostASkier/main/views.py
+++ HostASkier/main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from itertools import chain
 from host.models import Host
@@ -10,7 +11,7 @@ from skier.models import Skier
 def start(request):
     return render(request, 'main/start.html')
 
-class MatchView(ListView):
+class MatchView(LoginRequiredMixin, ListView):
     template_name = "match.html"
     context_object_name = "context"
 
