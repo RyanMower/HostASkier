b6b08a9bab5da38723ee9a5d45ec725f444e098f
Ryan Mower <ryancmower@gmail.com>
2021-06-23 21:08:01 -0500

    Authenticate on match page.

--- HostASkier/main/views.py
+++ HostASkier/main/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import AccessMixin
 from django.contrib import messages
 from itertools import chain
 from host.models import Host
 from skier.models import Skier
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+
 
 def start(request):
     return render(request, 'main/start.html')
 
-class MatchView(LoginRequiredMixin, ListView):
+class MatchView( AccessMixin,  ListView):
     template_name = "match.html"
     context_object_name = "context"
 
@@ -19,3 +22,16 @@ class MatchView(LoginRequiredMixin, ListView):
         queryset = {'hosts': Host.objects.all().filter(approved=True), 
                     'skiers': Skier.objects.all().filter(approved=True)}
         return queryset
+
+    def dispatch(self, request, *args, **kwargs):
+        authenticated = False
+        if request.user.is_authenticated:
+            authenticated = True
+            if request.user.approved:
+                return self.super.dispatch()
+                
+        if authenticated:
+            messages.error(request, "Your account is not approved!")
+        else:
+            messages.error(request, "Your are not signed in!")
+        return redirect('home')
